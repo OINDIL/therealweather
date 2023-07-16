90d860b1c09017c4f69ef40018aad12f32f07525
oindil <oindilgolder303@gmail.com>
2023-07-16 19:38:37 +0530

    fixed the search bar but the api call is not working on clicking the search button

--- src/Components/WeatherForm.js
+++ src/Components/WeatherForm.js
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import Textarea from './Textarea'
-
 export default function WeatherForm() {
   const [articles, setArticles] = useState(null)
-  const [search,setSearch] = useState('bangaon')
+  const [search,setSearch] = useState('')
   // const [totalResults, setTotalResults] = useState(0)
+  const updateWeather = async (query) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=a7e695993de0f4dd27081ffac1c6e754`
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    setArticles(parsedData)
+    console.log(parsedData)
+  }
   // using use effect hook for the first time
   function kelToCel(value){
     return (value - 273.15)
   }
+  const handleChange = (event) => {
+    // console.log(event.target.value)
+    // event.preventDefault()
+    setSearch(event.target.value)
+  }
+  const handleClick = () =>{
+    // alert('hello')
+    updateWeather(search)
+  }
+
   useEffect(() => {
-    const updateWeather = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a7e695993de0f4dd27081ffac1c6e754`
-      // const url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-06-14&sortBy=publishedAt&apiKey=fd09e8fd7ea240baa387a5d28a80b8f8'
-      let data = await fetch(url)
-      let parsedData = await data.json()
-      setArticles(parsedData)
-    }
-    updateWeather()
+    updateWeather('bongaon')
   }, [])
   // console.log(articles)
   return (
@@ -26,15 +35,15 @@ export default function WeatherForm() {
                 <div className="container">
                     <div className="container-fluid">
                         <form className="d-flex justify-content-center" role="search">
-                            <input className="form-control me-2" style={{ borderRadius: '20px', fontFamily: 'fira code', width: '50%' }} type="search" placeholder="Search" aria-label="Search" onChange={(event) => {setSearch(event.target.value)}} />
-                            <button className="btn btn-primary" style={{ borderRadius: '20px', fontFamily: 'fira code' }} type="submit">Search</button>
+                            <input className="form-control me-2" style={{ borderRadius: '20px', fontFamily: 'fira code', width: '50%' }} type="search" placeholder="Search" aria-label="Search" value={search} onChange={handleChange} />
+                            <button className="btn btn-primary" onClick={handleClick} style={{ borderRadius: '20px', fontFamily: 'fira code' }} type="submit">Search</button>
                         </form>
                     </div>
                 </div>
             </nav>
       {articles !== null ? (
         <div>
-          <Textarea icon={articles.weather[0].icon} name={articles.name} temp={Math.ceil(kelToCel(articles.main.temp))} description={articles.weather[0].description} />
+          <Textarea icon={articles.weather[0].icon} name={articles.name} temp={kelToCel(articles.main.temp).toFixed(1)} description={articles.weather[0].description} />
         </div>
       ) : (
         <div>
